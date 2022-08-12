8a9f3dc675b5b278585fd90eee9bdca0cb70b3fe
Deneux Benjamin <90443700+benjamin31200@users.noreply.github.com>
2022-08-12 13:36:07 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,26 +10,35 @@ non=n
 
 function start_project {
 
-	echo -n -e "${insertColor} On boss sur le projet ? y/n : ${norm}"
+	echo -n -e "${insertColor} Lancement du script de démarrage ? y/n ${norm}"
 	read y_n
 	if [ "$y_n" = "$yes" ]; then
-		echo -e "${answerColor} go, lancement du projet ! ${norm}"
-		#Changer la ligne suivante par votre projet actuel
-		code "$(cat ./savePath.sh)"
-		#Va lancer le projet sans aucunes commandes C  chaque fois.
-	elif [ "$y_n" = "$non" ]; then
-		#Lancement du projet principal, changer le projet principal ou lancement d'un projet spC)cifique.
-		echo -n -e "${insertColor} Voulez-vous configurer un nouveau projet à la place de celui existant ? y/n ${norm}"
+		#Lancement du serveur mysql
+		echo -e "${answerColor} démarrage du serveur mysql... ${norm}"
+		sudo service mysql start
+		echo -e "${answerColor} lancement de mysql ok. ${norm}"
+		#Lancement du projet ou d'un projet spécifique ?.
+		echo -n -e "${insertColor} Lancer le projet principal ? y/n ${norm}"
 		read y_n
 		if [ "$y_n" = "$yes" ]; then
-			bash "$(find ./ -name search_dir.sh)"
+			echo -e "${answerColor} Lancement du projet ! ${norm}"
+			#Changer la ligne suivante par votre projet actuel
+			code "$(cat "$(find ./ -name savePath.sh)")"
+			#Va lancer le projet sans aucunes commandes C  chaque fois.
 		elif [ "$y_n" = "$non" ]; then
-			echo -e "${answerColor}Projet conservé, sorti du programme.${norm}"
+			#Lancement du projet principal, changer le projet principal ou lancement d'un projet spC)cifique.
+			echo -n -e "${insertColor} Voulez-vous configurer un nouveau projet à la place de celui existant ? y/n ${norm}"
+			read y_n
+			if [ "$y_n" = "$yes" ]; then
+				bash "$(find ./ -name search_dir.sh)"
+			elif [ "$y_n" = "$non" ]; then
+				echo -e "${answerColor}Projet conservé, sorti du programme.${norm}"
+			fi
 		fi
+	elif [ "$y_n" = "$non" ]; then
+		echo -e "${answerColor} Bonne session ! ${norm}"
 	fi
 
 }
-
 start_project
-
 exit 0
