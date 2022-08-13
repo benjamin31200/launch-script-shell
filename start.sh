cf85ee82c93ed308b2b8326d58074e9979affbdf
Deneux Benjamin <90443700+benjamin31200@users.noreply.github.com>
2022-08-13 12:43:36 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
-insertColor=$(echo -en '\033[1;33;4m')
-answerColor=$(echo -en '\033[01;35m')
-errorColor=$(echo -en '\033[01;31m')
-indicationColor=$(echo -en '\033[3;32m')
-hugeIndicationColor=$(echo -en '\033[5;37;41m')
-successColor=$(echo -en '\033[01;32m')
-norm=$(echo -en '\033[0m')
+set -o posix
+insertColor='\033[1;33;4m'
+answerColor='\033[01;35m'
+indicationColor='\033[3;32m'
+hugeIndicationColor='\033[5;37;41m'
+norm='\033[0m'
 yes=y
 non=n
-next=$'\n'
-doubleNext=$'\n\n'
-tab=$'\t\t'
+n='\n'
+doubleNext='\n\n'
+tab='\t\t'
 
 function start_project {
 
-	echo -e "${answerColor} démarrage du serveur mysql... ${norm}"
+	printf "$answerColor démarrage du serveur mysql... %s$n$norm"
 	sudo service mysql start
-	echo -e "${answerColor} lancement de mysql ok. ${norm}"
-	echo -n -e "${insertColor} Lancer le projet principal ? y/n ${norm}"
+	printf "$answerColor lancement de mysql ok. %s$n$norm"
+	echo -n -e "$insertColor Lancer le projet principal ? y/n $norm"
 	read y_n
 	if [ "$y_n" = "$yes" ]; then
-		echo -e "${answerColor} Lancement du projet ! ${norm}"
+		printf "$answerColor Lancement du projet ! %s$n$norm"
 		code "$(cat "$(find ./ -name mainProject.sh)")"
 	elif [ "$y_n" = "$non" ]; then
-		echo -n -e "${insertColor} Suivre les consignes: ${norm}"
-		echo -e "${doubleNext}${tab}${indicationColor} Choisir l'une des options en tapant le mot en surlignement: ${next}${norm}"
-		echo -e "${tab}${indicationColor} Pour paramétrer un projet existant, taper: ${norm}${hugeIndicationColor}old${norm}"
-		echo -e "${tab}${indicationColor} Pour paramétrer un nouveau projet, taper: ${norm}${hugeIndicationColor}new${norm}"
-		echo -e "${tab}${indicationColor} Pour lancer un projet spécifique, taper: ${norm}${hugeIndicationColor}project${norm}"
-		echo -e "${tab}${indicationColor} Pour changer de projet principal, taper: ${norm}${hugeIndicationColor}main${norm}"
-		echo -e "${tab}${indicationColor} Pour quitter le script, taper: ${norm}${hugeIndicationColor}exit${norm}"
+		echo -n -e "$insertColor Suivre les consignes: $norm"
+		printf "$doubleNext$tab$indicationColor Choisir l'une des options en tapant le mot en surlignement: %s$n$norm"
+		printf "$tab$indicationColor Pour paramétrer un projet existant, taper: $norm${hugeIndicationColor}old%s$n$norm"
+		printf "$tab$indicationColor Pour paramétrer un nouveau projet, taper: $norm${hugeIndicationColor}new%s$n$norm"
+		printf "$tab$indicationColor Pour lancer un projet spécifique, taper: $norm${hugeIndicationColor}project%s$n$norm"
+		printf "$tab$indicationColor Pour changer de projet principal, taper: $norm${hugeIndicationColor}main%s$n$norm"
+		printf "$tab$indicationColor Pour quitter le script, taper: $norm${hugeIndicationColor}exit%s$n$norm"
 		read y_n
-		case "${y_n}" in
+		case "$y_n" in
 		old)
 			bash "$(find ./ -name old.sh)"
 			;;
@@ -45,8 +44,8 @@ function start_project {
 			echo "en construction"
 			;;
 		exit)
-			echo -e "${answerColor} Sortie du script. ${norm}"
-			echo -e "${answerColor} Bonne session ! ${norm}"
+			printf "$answerColor Sortie du script. %s$n$norm"
+			printf "$answerColor Bonne session ! %s$n$norm"
 			exit 0
 			;;
 			*)
@@ -55,7 +54,7 @@ function start_project {
 	else
 		start_project
 	fi
-
 }
+
 start_project
 exit 0
